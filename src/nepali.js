eef19ae29fd61489fb9d29e1c589edb6d0c47e69
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2019-12-21 11:42:00 +0545

    fix bug by setting cursor position after input changes

--- src/nepali.js
+++ src/nepali.js
@@ -1,34 +1,51 @@
 import React, { Component } from 'react';
-import './nepali-mapping';
+import { mappingFunction } from './nepali-mapping';
 
 export class Nepali extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      value: ''
+      value: this.props.initialValue || ''
     };
   }
 
-  calculate(e){
+  calculate(e) {
     let value = "";
-    if(this.state.value !== e.target.value){
-      for(let c of e.target.value){
-        let conv_char = window[this.props.funcname](c.charCodeAt(0));
+    e.persist();
+    if (this.state.value !== e.target.value) {
+      for (let c of e.target.value) {
+        let conv_char = mappingFunction[this.props.funcname](c.charCodeAt(0));
         value += conv_char || c;
       }
       this.setState({ value });
+      this.adjustCursor(e.target);
       this.props.valueChange && this.props.valueChange(e, value);
     }
   }
 
+  adjustCursor(inputRef) {
+    const selectionStart = inputRef.selectionStart;
+    // don't override event instead let event play and then 
+    // set the selection range after 10ms, bit hacky but works
+    setTimeout(() => {
+      inputRef.setSelectionRange(selectionStart, selectionStart);
+    }, 10);
+  }
+
   render() {
-    return (
-      <input
-        onChange={ this.calculate.bind(this) }
-        value={this.state.value}
-        { ...this.props }
-      />
-    );
+    const { inputType, ...props } = this.props;
+    return inputType === 'textarea' ? (
+      <textarea
+        {...props}
+        onChange={this.calculate.bind(this)}
+        value={this.state.value} ></textarea>
+    ) : (
+        <input
+          {...props}
+          onChange={this.calculate.bind(this)}
+          value={this.state.value}
+        />
+      );
   }
 }
 
