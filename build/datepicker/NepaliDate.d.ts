a6ab28c92adadcd0fe99cf99f1fbee708ed447bf
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-04-22 10:10:58 +0545

    add nepali date object

---
+++ build/datepicker/NepaliDate.d.ts
@@ -0,0 +1,65 @@
+interface INepaliDateFormat {
+    year: string;
+    month: string;
+    day: string;
+    strMonth: string;
+    strShortMonth: string;
+    dayOfWeek: string;
+    strDayOfWeek: string;
+    strShortDayOfWeek: string;
+    totalDaysInMonth: string;
+}
+/**
+ * Nepali Date class that includes some additional functionality
+ * to the date object like formatting.
+ */
+declare class NepaliDate {
+    private en;
+    private ne;
+    private dateObject;
+    formatKeyMap: {
+        y: string;
+        m: string;
+        d: string;
+        M: string;
+        sM: string;
+        w: string;
+        W: string;
+        sW: string;
+        tdm: string;
+    };
+    adDateStr: string;
+    adBsObject: {
+        en: INepaliDateFormat;
+        ne: INepaliDateFormat;
+    };
+    /**
+     *
+     * @param bsDate {string} in format `yyyy/mm/dd`
+     */
+    constructor(dateInput?: string | Date, type?: "ad" | "bs");
+    /**
+     * To get internal javascript date object
+     *
+     * @returns {Date} javascript date object
+     */
+    getDateObject: () => Date;
+    /**
+     * get `ADBSConverter` date object
+     *
+     */
+    getADBSObject: () => {
+        en: INepaliDateFormat;
+        ne: INepaliDateFormat;
+    };
+    /**
+     * typescript hasKey https://dev.to/kingdaro/indexing-objects-in-typescript-1cgi
+     */
+    private hasKey;
+    /**
+     * Format the date
+     */
+    format: (formatStr: string, type?: "en" | "ne") => string;
+}
+export default NepaliDate;
+export { INepaliDateFormat };
---
+++ src/datepicker/NepaliDate.ts
@@ -0,0 +1,133 @@
+import { ADBSConverter } from ".";
+
+interface INepaliDateFormat {
+    year: string,
+    month: string,
+    day: string,
+    strMonth: string,
+    strShortMonth: string,
+    dayOfWeek: string,
+    strDayOfWeek: string,
+    strShortDayOfWeek: string,
+    totalDaysInMonth: string
+}
+
+/**
+ * Nepali Date class that includes some additional functionality 
+ * to the date object like formatting.
+ */
+class NepaliDate {
+    
+
+    private en: INepaliDateFormat;
+
+    private ne: INepaliDateFormat;
+
+    private dateObject: {
+        en: INepaliDateFormat,
+        ne: INepaliDateFormat
+    };
+
+    formatKeyMap = {
+        'y': 'year',
+        'm': 'month',
+        'd': 'day',
+        'M': 'strMonth',
+        'sM': 'strShortMonth',
+        'w': 'dayOfWeek',
+        'W': 'strDayOfWeek',
+        'sW': 'strShortDayOfWeek',
+        'tdm': 'totalDaysInMonth'
+    }
+
+    adDateStr: string;
+
+    adBsObject: {
+        en: INepaliDateFormat,
+        ne: INepaliDateFormat
+    }
+
+    /**
+     * 
+     * @param bsDate {string} in format `yyyy/mm/dd`
+     */
+    constructor(dateInput: string | Date = null, type: "ad" | "bs" = "ad") {
+        let today = new Date();
+        let dateStr;
+
+        if (dateInput === null) {
+            const date = new Date();
+            dateStr = `${date.getFullYear()}/${date.getMonth() + 1}/${today.getDate()}`
+        } else {
+            if (dateInput.constructor === Date) {
+                today = dateInput as Date;
+                dateStr = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`
+            }
+            if (dateInput.constructor === String) {
+                if (type === "bs") {
+                    const adDate = ADBSConverter.bs2ad(dateInput as string);
+                    dateStr = `${adDate.year}/${adDate.month}/${adDate.day}`;
+                }
+                if (type === "ad") {
+                    dateStr = dateInput as string;
+                }
+            }
+        }
+
+        this.adDateStr = dateStr;
+    }
+
+    /**
+     * To get internal javascript date object
+     * 
+     * @returns {Date} javascript date object
+     */
+    getDateObject = () => {
+        const [year, month, day] = this.adDateStr.split("/").map(d => Number(d));
+        const javascriptDate = new Date(year, month - 1, day);
+        return javascriptDate;
+    }
+
+    /**
+     * get `ADBSConverter` date object
+     * 
+     */
+    getADBSObject = () => {
+        if(!this.adBsObject){
+            this.adBsObject = ADBSConverter.ad2bs(this.adDateStr);
+        }
+        return this.adBsObject;
+    }
+
+
+    /**
+     * typescript hasKey https://dev.to/kingdaro/indexing-objects-in-typescript-1cgi
+     */
+    private hasKey<T>(obj: T, key: keyof any): key is keyof T {
+        return key in obj;
+    }
+
+    /**
+     * Format the date
+     */
+    format = (formatStr: string, type: "en" | "ne" = "en"): string => {
+        let formattedString = formatStr.replace(/\{[^\{\}]*\}/g, (val) => {
+            val = val.substr(1, val.length - 2);
+            let [value, padding] = val.split("|");
+            if(this.hasKey(this.formatKeyMap, value)){
+                const key = this.formatKeyMap[value] as keyof INepaliDateFormat;
+                const adBsObject = this.getADBSObject();
+                return adBsObject[type][key];
+            }
+            return val;
+        })
+        return formattedString;
+    }
+
+
+}
+
+export default NepaliDate;
+export {
+    INepaliDateFormat
+}
